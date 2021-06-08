0761c80c12e34bd162b27e13b0190fc0b16d6dc7
heyjohnis <juni0227@nate.com>
2021-06-08 23:10:37 +0900

    axios 를 활용, fatch 대신

--- youtube/src/service/youtube.js
+++ youtube/src/service/youtube.js
@@ -1,28 +1,50 @@
+import axios from 'axios';
 class Youtube {
-  
-
   constructor(key) {
-    this.key = key;
-    this.requestOptions = {
-      method: 'GET',
-      redirect: 'follow'
-    };
-    this.channelId = 'UC2fP4GE9B6O8_T8Lp82fnJQ'; //UC2fP4GE9B6O8_T8Lp82fnJQ 대방, UC0Qzngib6BwuBGoRZpL9YEw 전주
-    this.url = `https://www.googleapis.com/youtube/v3/search?key=${this.key}&part=snippet&channelId=${this.channelId}&maxResults=20&order=date`;
+    this.youtube = axios.create({
+      baseURL: 'https://www.googleapis.com/youtube/v3/',
+      params: {key: key},
+    })
+    // this.key = key;
+    // this.requestOptions = {
+    //   method: 'GET',
+    //   redirect: 'follow'
+    // };
+    //this.channelId = 'UC2fP4GE9B6O8_T8Lp82fnJQ'; //UC2fP4GE9B6O8_T8Lp82fnJQ 대방, UC0Qzngib6BwuBGoRZpL9YEw 전주
+    //this.url = `https://www.googleapis.com/youtube/v3/search?key=${this.key}&part=snippet&channelId=${this.channelId}&maxResults=20&order=date`;
   }
 
   async movieList() {
-    const response = await fetch(this.url,
-      this.requestOptions);
-    const result_1 = await response.json();
-    return result_1.items;
+    const response = await this.youtube.get('search', {
+      params: {
+        part: 'snippet',
+        channelId: 'UC2fP4GE9B6O8_T8Lp82fnJQ',
+        maxResults: 20,
+        order: 'date',
+      }
+    });
+    return response.data.items;
+    // const response = await fetch(this.url,
+    //   this.requestOptions);
+    // const result_1 = await response.json();
+    // return result_1.items;
   }
 
   async searchMovieList(query) {
-      const response = await fetch(`${this.url}&q=${query}`,
-        this.requestOptions);
-      const result_1 = await response.json();
-      return result_1.items;
+    const response = await this.youtube.get('search', {
+      params: {
+        part: 'snippet',
+        channelId: 'UC2fP4GE9B6O8_T8Lp82fnJQ',
+        maxResults: 20,
+        order: 'date',
+        q: query
+      }
+    });
+    return response.data.items;
+      // const response = await fetch(`${this.url}&q=${query}`,
+      //   this.requestOptions);
+      // const result_1 = await response.json();
+      // return result_1.items;
   }
 
 }
