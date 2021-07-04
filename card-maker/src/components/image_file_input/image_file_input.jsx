35f8275e990f99ccdd7f347fb2fb44d47ba9a169
heyjohnis <juni0227@nate.com>
2021-07-04 12:53:39 +0900

    card : Loding Spinner 추가

--- card-maker/src/components/image_file_input/image_file_input.jsx
+++ card-maker/src/components/image_file_input/image_file_input.jsx
@@ -1,10 +1,13 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import styles from './image_file_input.module.css';
 
 const ImageFileInput = ({ imageUploader, name, onFileChange }) => {
+  const [loading, setLoading] = useState(false);
   const inputRef = useRef();
   const onChange = async event => {
+    setLoading(true);
     const uploaded = await imageUploader.upload(event.target.files[0]);
+    setLoading(false);
     onFileChange({
       name: uploaded.original_filename,
       url: uploaded.url
@@ -18,9 +21,12 @@ const ImageFileInput = ({ imageUploader, name, onFileChange }) => {
   return (
     <div className={styles.container}>
       <input ref={inputRef} className={styles.input} type="file" accept="image/*" name="file" onChange={onChange} />
-      <button className={styles.button} onClick={onButtonClick}>
+      { !loading && (
+      <button className={`${styles.button} ${name ? styles.pink : styles.grey}`} onClick={onButtonClick}>
         {name || 'No file' }
-      </button>
+      </button>)
+      }
+      { loading && <div className={styles.loading}></div> }
     </div>
   );
 };
